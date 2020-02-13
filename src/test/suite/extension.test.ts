77583ece5b9284bd65d1aa21859ee69cd6f685f9
chelsea.lo@pentium.network <chelsealopentinum>
2020-02-13 18:45:51 +0800

    misc: add extension basic test.

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,16 +1,35 @@
 import * as assert from "assert";
 import { suite, test } from "mocha";
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vscode from "vscode";
-// import * as myExtension from '../extension';
 
 suite("Extension Test Suite", () => {
+    const extensionId = "pentium.mflow-extension";
     vscode.window.showInformationMessage("Start all tests.");
 
-    test("Sample test", () => {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
-        assert.equal(-1, [1, 2, 3].indexOf(0));
+    test("should be present", () => {
+        assert.ok(vscode.extensions.getExtension(extensionId));
+    });
+
+    test("Should be able to register mflow commands", async () => {
+        const commands = await vscode.commands.getCommands(true);
+        const MFLOW_COMMANDS = [
+            "mflow.show.version",
+            "mflow.create.project",
+            "mflow.blcks.create",
+            "mflow.ansible.create",
+            "mflow.shell.create",
+            "mflow.build.base",
+            "mflow.install.script",
+            "mflow.uninstall.script",
+            "mflow.up",
+            "mflow.down",
+            "mflow.run",
+            "mflow.logs"
+        ];
+        const foundArduinoCommands = commands.filter(value => {
+            return MFLOW_COMMANDS.indexOf(value) >= 0 || value.startsWith("mflow.");
+        });
+        const errorMsg = "Some mflow commands are not registered properly or a new command is not added to the test";
+        assert.equal(foundArduinoCommands.length, MFLOW_COMMANDS.length, errorMsg);
     });
 });
