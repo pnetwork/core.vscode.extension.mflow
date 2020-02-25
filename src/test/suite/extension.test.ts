cd77855453a77263223c543ba2b46d714a101291
chelsea.lo@pentium.network <chelsealopentinum>
2020-02-25 16:23:09 +0800

    misc: fix test command count.

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -27,6 +27,7 @@ suite("Extension Test Suite", function() {
             "mflow.show.installed",
             "mflow.install.script",
             "mflow.uninstall.script",
+            "mflow.view.wf",
             "mflow.up",
             "mflow.run",
             "mflow.down",
